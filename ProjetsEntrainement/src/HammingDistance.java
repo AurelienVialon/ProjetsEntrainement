e612321de14273b55a9c926c55de9425ac74d6b1
Aurélien <Aurlien@DESKTOP-T3T9QQU>
2017-03-17 22:33:58 +0900

    Modif de la distance de Hamming

--- ProjetsEntrainement/src/HammingDistance.java
+++ ProjetsEntrainement/src/HammingDistance.java
@@ -2,7 +2,7 @@ public class HammingDistance
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hamming Distance est : " + HammingDistance(1,4));
+		System.out.println("Hamming Distance est : " + HammingDistance(3,1));
 	}
 	
 	public static int HammingDistance(int x, int y)
@@ -10,15 +10,22 @@ public class HammingDistance
 			String bx = Integer.toBinaryString(x), by = Integer.toBinaryString(y);
 			int h = 0;
 			
+			System.out.println(bx);
+			System.out.println(by);
+			
 			if(bx.length() != by.length())
 			{		
 				if(bx.length()<by.length())
 					while(bx.length()<by.length())
 						bx="0" + bx;
 				else
-					while(bx.length()<by.length())
+					while(bx.length()>by.length())
 						by="0" + by;
 			}
+			
+			System.out.println(bx);
+			System.out.println(by);
+			
 			
 			while(!bx.equals("") && !by.equals(""))
 			{	
